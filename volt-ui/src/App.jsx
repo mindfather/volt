b624fae0db00169bba7062df15812a6fe48bbf58
~mipsun-diflex <ed@edwardflex.info>
2022-07-29 14:17:53 -0500

    Initial Commit for volt-ui
    - Add Front End Project
    - Include `.env` (for now) with variables for testnet
    - Edit docket to take a glob
    - Attempt to poke `%volt`

---
+++ volt-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ volt-ui/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  base: '/apps/volt/',
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+//      external: [
+//        "/session.js"
+//      ]
+    }
+  },
+});
